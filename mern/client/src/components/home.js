b8592e207892559e19f125757c279a3be61a9499
Alex Kaszynski <alex.kaszynski@gmail.com>
2022-05-22 14:28:00 -0500

    update 0.19 Home Updated & Icon Added

--- mern/client/src/components/home.js
+++ mern/client/src/components/home.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "../myStyle.css";
+import profile from "../images/IMG-3724.png";
 //import { useNavigate } from "react-router";
 
 export default function Login() {
   return (
-    <div className="home-content">
-     Home Text
+    <div>
+      <div className="home-title">
+      Home
+      </div>
+      <div className="home-content">
+        Hello, my name is Alex Kaszynski. Thank you for visiting my website.
+        Feel free to visit the About page to learn more about me and what I do,
+        or the Projects page to see what I've been working on lately, or
+        even the Gallery to discover what I make in my spare time!
+      </div>
     </div>
   );
 }
--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -1,36 +1,47 @@
 import React from "react";
 import "../myStyle.css"; // We import bootstrap to make our application look better.
+import logo from "../images/Tetra.png";
 import { NavLink } from "react-router-dom"; // We import NavLink to utilize the react router.
+//  <div className="nav-home"> Home </div>
 
 export default function Navbar() {
   return (
-    <header >
-      
-    <div className="navbar-container" id="nav-container">
-      <NavLink to="/">
-        <div className="nav-home">Home</div>
-      </NavLink>
+    <header>
+      <div className="navbar-container" id="nav-container">
+        <img
+          src={logo}
+          className="nav-logo"
+          alt="Home-Logo-Made-by-Dmitriy-Malayev"
+        />
 
-      <nav className="nav">
-        <ul>
-          <NavLink to="/about">
-            <li>About Me</li>
-          </NavLink>
+        <nav className="nav">
+          <ul>
+            <NavLink to="/">
+              <li>Home</li>
+            </NavLink>
 
-          <NavLink to="/gallery">
-            <li>Gallery</li>
-          </NavLink>
+            <NavLink to="/about">
+              <li>About</li>
+            </NavLink>
 
-          <NavLink to="/projects">
-            <li>Projects</li>
-          </NavLink>
+            <NavLink to="/gallery">
+              <li>Gallery</li>
+            </NavLink>
 
-          <NavLink to="/placeholder">
-            <li>Place-Holder</li>
-          </NavLink>
-        </ul>
-      </nav>
-    </div>
+            <NavLink to="/projects">
+              <li>Projects</li>
+            </NavLink>
+
+            <NavLink to="/contact">
+              <li>Contact</li>
+            </NavLink>
+
+            <NavLink to="/placeholder">
+              <li>Place-Holder</li>
+            </NavLink>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
