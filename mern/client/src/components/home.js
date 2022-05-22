88ad137258ff8b5db9fee815f08018d4bfcc4fbd
Alex Kaszynski <alex.kaszynski@gmail.com>
2022-05-22 14:29:48 -0500

    update 0.19.5 Updated CSS & Home Todo List

--- mern/client/src/components/home.js
+++ mern/client/src/components/home.js
@@ -6,14 +6,27 @@ import profile from "../images/IMG-3724.png";
 export default function Login() {
   return (
     <div>
-      <div className="home-title">
-      Home
-      </div>
+      <div className="home-title">Home</div>
       <div className="home-content">
         Hello, my name is Alex Kaszynski. Thank you for visiting my website.
         Feel free to visit the About page to learn more about me and what I do,
-        or the Projects page to see what I've been working on lately, or
-        even the Gallery to discover what I make in my spare time!
+        or the Projects page to see what I've been working on lately, or even
+        the Gallery to discover what I make in my spare time!
+      </div>
+
+      <div className="home-todo">
+        Website ToDo List:<br></br>
+        - make links for the different pages using router
+        <br />
+        - make footer elements that link to different social sites (LinkedIn,
+        Github, Handshake, etc.) that stick to the bottom of the page
+        <br />
+        - Fill in about page with resume information
+        <br />
+        - Have links in Projects page linking to my different public Github Projects
+        <br />
+        - Contact can be deleted if/once sticky footer elements are installed
+        <br />
       </div>
     </div>
   );
